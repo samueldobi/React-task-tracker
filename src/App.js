9c5cfa672817de9d6d7b1ecbb8823aa24b52c216
samueldobi <frankdobisamuel@gmail.com>
2023-01-06 12:48:58 +0100

    Added Styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -23,18 +24,28 @@ function App() {
         reminder:false,
     }
 ])
+//Add Task 
+const addTask = (task)=>{
+  console.log(task)
+}
 //Delete Task
 const deleteTask = (id) =>{
   setTasks(tasks.filter((task)=> task.id !==id))
 }
 //Toggle Reminder
-const toggleReminder = (id) =>{}
+const toggleReminder = (id) =>{
+  setTasks(tasks.map((task)=> task.id === id 
+  ? {...task , reminder: !task.reminder} : task))
+}
+
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd ={addTask}/>
       {tasks.length>0 ? ( <Tasks 
       tasks = {tasks} 
       onDelete = {deleteTask}
+      onToggle = {toggleReminder}
       />) :('No Tasks to show')}
     </div>
   );
